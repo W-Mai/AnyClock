116da74a88990d2c66ac239318c47dea7c288dfe
Benign X <1341398182@qq.com>
2023-11-12 02:51:33 +0800

    💓(Widget): add widget template
    Signed-off-by: Benign X <1341398182@qq.com>

---
+++ Number/NumberBundle.swift
@@ -0,0 +1,16 @@
+//
+//  NumberBundle.swift
+//  Number
+//
+//  Created by W-Mai on 2023/11/12.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NumberBundle: WidgetBundle {
+    var body: some Widget {
+        Number()
+    }
+}
